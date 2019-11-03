2d61c654e06846fed6c00731a5889b214a29f827
hritikchokker <hchokker7@gmail.com>
2019-11-03 11:53:17 +0530

    videoList Added

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import SearchBar from './searchBar';
 import youtube, { KEY } from '../apis/youtube';
+import VideoList from './videoList';
 
 class App extends React.Component{
     state = {videos: []};
     onTermSubmit = async term => {
-       const response = await youtube.get('/search',{
+        const response = await youtube.get('/search',{
             params: {
-               part: 'snippet',
+                part: 'snippet',
                 q: term,
-               maxResults: 5,
-               key: KEY.url  
+                maxResults: 5,
+                key: KEY.url  
             }
         })
-        this.state({videos: response.data.items})
+        this.setState({videos: response.data.items})
         console.log(response,"response");
         
     }
-    render(){
+    onVideoSelect = (video) =>{
+        console.log('from the app',video);
+        
+    }
+    render() {
         return (
-        <div className="ui container">
-         <SearchBar onFormSubmit={this.onTermSubmit} />
-         </div>
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
         )
     }
 }
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import VideoItem from './videoItem';
 
-const VideoList = props => {
+const VideoList = ({videos,onVideoSelect}) => {
+    const renderedList = videos.map((video)=>{
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    })
     //props.video
-    return <div>VideoList</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 }
-export default VideoList;
+export default VideoList; 
